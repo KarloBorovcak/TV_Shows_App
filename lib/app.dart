15f83b7aa55b4d3702aa6cd003aa0ee42afde848
Karlo_Borovcak <borovcak.karlo@gmail.com>
2022-03-15 16:49:45 +0100

    First push

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class TvShowsApp extends StatelessWidget {
+  const TvShowsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: HomeScreen(),
+    )
+  }
+
+
+}
